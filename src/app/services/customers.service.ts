5dbaaf5e23ba614dc5d5aafb9daab7ce098141ea
RobertoCarrera <robertocarrera93@gmail.com>
2025-02-26 20:19:18 +0100

    Add Customers service

---
+++ src/app/services/customers.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/customer';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomersService {
+
+  private apiUrl = "https://a2022.twidget.io/clientes";
+  
+  constructor(private http: HttpClient){}
+
+  getCustomers(): Observable<Customer[]>{
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+
+  createCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer);
+  }
+
+  updateCustomer(customerId: string, updateData: any): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${customerId}`, updateData);
+  }
+
+  deleteCustomer(customerId: string): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/${customerId}`);
+  }
+}
