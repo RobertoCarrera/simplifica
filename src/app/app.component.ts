b2a14b308bd9693ed177ffa8ff6d148dfc4faf58
RobertoCarrera <robertocarrera93@gmail.com>
2025-02-26 12:58:33 +0100

    Add sidebar component with everything needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { SidebarComponent } from './utils/sidebar/sidebar.component';
+import { SidebarService } from './services/sidebar.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'simplifica';
+  constructor(public sidebarService: SidebarService){}
 }
