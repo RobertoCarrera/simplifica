3b3247c3e66b41cab99280dbc6dbe361322e0d3a
RobertoCarrera <robertocarrera93@gmail.com>
2025-02-26 12:48:34 +0100

    Fix NGclass working with CommonModule import in sidebar.ts

--- src/app/utils/sidebar/sidebar.component.ts
+++ src/app/utils/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
