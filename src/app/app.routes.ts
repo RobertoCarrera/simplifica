1b8da2ef926d441578eba72696f50373604fdaa5
RobertoCarrera <robertocarrera93@gmail.com>
2025-03-02 12:34:30 +0100

    Add component + add to the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardCustomersComponent } from './components/dashboard-customers/dashboard-customers.component';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 import { DashboardWorkshopComponent } from './components/dashboard-workshop/dashboard-workshop.component';
+import { DashboardSatComponent } from './components/dashboard-sat/dashboard-sat.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: DashboardHomeComponent},
+    {path: 'sat', component: DashboardSatComponent},
     {path: 'taller', component: DashboardWorkshopComponent},
     {path: 'clientes', component: DashboardCustomersComponent},
     {path: '', redirectTo: '/inicio', pathMatch: 'full'}
---
+++ src/app/components/dashboard-sat/dashboard-sat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sat',
+  imports: [],
+  templateUrl: './dashboard-sat.component.html',
+  styleUrl: './dashboard-sat.component.scss'
+})
+export class DashboardSatComponent {
+
+}
