58df200ff8c1d98d7724cb07f89f6ad564c83ee9
RobertoCarrera <robertocarrera93@gmail.com>
2026-02-05 01:52:14 +0100

    Corregido responsive de webmail

--- src/app/features/webmail/webmail-layout/webmail-layout.component.ts
+++ src/app/features/webmail/webmail-layout/webmail-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { FolderTreeComponent } from '../components/folder-tree/folder-tree.component';
 import { WebmailSettingsComponent } from '../components/settings/webmail-settings.component';
 import { MailStoreService } from '../services/mail-store.service';
@@ -16,6 +16,16 @@ export class WebmailLayoutComponent implements OnInit {
   private store = inject(MailStoreService);
 
   showSettings = signal(false);
+  isSidebarOpen = signal(false);
+
+  private router = inject(Router);
+
+  constructor() {
+    // Close sidebar on navigation (mobile)
+    this.router.events.subscribe(() => {
+      this.isSidebarOpen.set(false);
+    });
+  }
 
   ngOnInit() {
     this.store.loadAccounts();
@@ -23,5 +33,13 @@ export class WebmailLayoutComponent implements OnInit {
 
   toggleSettings() {
     this.showSettings.update(v => !v);
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen.update(v => !v);
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen.set(false);
   }
 }
