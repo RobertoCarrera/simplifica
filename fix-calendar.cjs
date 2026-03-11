633aadf89942264ed9f894a781f3cf0617df6a6c
RobertoCarrera <robertocarrera93@gmail.com>
2026-03-11 17:38:52 +0100

    Recuperad a centralización de la línea de hora actual en el centro de la vista

--- fix-calendar.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = 'f:/simplifica-copilot/src/app/features/calendar/calendar.component.ts';
-let src = fs.readFileSync(path, 'utf8');
-
-src = src.replace('currentDayEvents() { return this.getEventsForDate(this.currentView().date); }', '');
-
-fs.writeFileSync(path, src);
